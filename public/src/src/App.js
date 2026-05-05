c9d1ed7daa0152a70c63228a4438262eaeb21305
Sanjana121993 <sbalakrishnan@zuora.com>
2026-05-05 16:37:45 +0530

    Create App.js

---
+++ public/src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PaymentsAnalyticsDashboard from './components/PaymentsAnalyticsDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <PaymentsAnalyticsDashboard />
+    </div>
+  );
+}
+
+export default App;
